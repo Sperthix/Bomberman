3da2e3cdd39ff2548b105ffc296168e639a73b1e
Filip Agh <filip11agh@gmail.com>
2025-11-18 22:05:29 +0100

    base character movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int walkSpeed = 15;
+    public int sensitivity = 10;
+
+    private CharacterController CharacterController;
+    private InputAction moveAction;
+    private InputAction lookAction;
+
     void Start()
     {
+        CharacterController = GetComponent<CharacterController>();
+        moveAction = InputSystem.actions.FindAction("Move");
+        lookAction = InputSystem.actions.FindAction("Look");
         
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        var moveInput = moveAction.ReadValue<Vector2>();
+        var moveVector = transform.forward * moveInput.y + transform.right * moveInput.x;
+        CharacterController.Move(moveVector * Time.deltaTime * walkSpeed);
+
+        var lookInput = lookAction.ReadValue<Vector2>();
+        transform.Rotate(Vector3.up, lookInput.x * Time.deltaTime * sensitivity);
     }
 }
