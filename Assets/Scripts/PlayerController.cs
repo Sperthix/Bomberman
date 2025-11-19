0ca23122fdd3c2eb908e6a6bba39f322f0cf9353
Sperthix <matusbuci@gmail.com>
2025-11-19 16:34:00 +0100

    placing bombs on pressing E

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,25 @@ public class PlayerController : MonoBehaviour
 {
     public int walkSpeed = 15;
     public int sensitivity = 10;
+    public int bombSpawnDistance = 1;
 
-    private CharacterController CharacterController;
+    public GameObject bombPrefab;
+
+    private CharacterController characterController;
+    private PlayerInput playerInput;
+    
     private InputAction moveAction;
     private InputAction lookAction;
+    private InputAction placeBombAction;
 
     void Start()
     {
-        CharacterController = GetComponent<CharacterController>();
-        moveAction = InputSystem.actions.FindAction("Move");
-        lookAction = InputSystem.actions.FindAction("Look");
+        characterController = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+        
+        moveAction = playerInput.actions["Move"];
+        lookAction = playerInput.actions["Look"];
+        placeBombAction = playerInput.actions["PlaceBomb"];
         
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -22,11 +31,45 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        HandleMovement();
+        HandleLook();
+        HandleBombPlacement();
+    }
+
+    private void HandleBombPlacement()
+    {
+        if (placeBombAction.WasPerformedThisFrame())
+        {
+            PlaceBomb();
+        }
+    }
+
+    void HandleMovement()
+    {
         var moveInput = moveAction.ReadValue<Vector2>();
         var moveVector = transform.forward * moveInput.y + transform.right * moveInput.x;
-        CharacterController.Move(moveVector * Time.deltaTime * walkSpeed);
+        characterController.Move(moveVector * (Time.deltaTime * walkSpeed));
+    }
 
+    void HandleLook()
+    {
         var lookInput = lookAction.ReadValue<Vector2>();
         transform.Rotate(Vector3.up, lookInput.x * Time.deltaTime * sensitivity);
+    }
+
+    void PlaceBomb()
+    {
+        if (bombPrefab is null)
+        {
+            Debug.LogWarning("Nemam bombu");
+            return;
+        }
+        
+        Vector3 center = characterController.bounds.center;
+        Vector3 forward = transform.forward;
+        Vector3 PlacePos = center + forward * bombSpawnDistance;
+        PlacePos.y = 0.5f;
+        
+        Instantiate(bombPrefab, PlacePos, Quaternion.identity);
     }
 }
