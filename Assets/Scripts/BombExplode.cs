6bfc05ff223f64b9e46464fdf5094f504529e86e
Filip Agh <filip11agh@gmail.com>
2025-11-22 16:36:15 +0100

    make player prefab and use it in scene add 4way linear bomb

--- Assets/Scripts/BombExplode.cs
+++ Assets/Scripts/BombExplode.cs
@@ -1,10 +1,19 @@
-using UnityEngine;
 using System.Collections;
+using System.Linq;
+using UnityEngine;
 
 public class BombExplode : MonoBehaviour
 {
     [SerializeField] private float fuseTime = 3f;
     [SerializeField] private float radius = 2f;
+    public BombType bombType;
+
+    private GameState _gs;
+
+    private void Start()
+    {
+        _gs = GameState.Instance;
+    }
 
     private void OnEnable()
     {
@@ -14,10 +23,49 @@ public class BombExplode : MonoBehaviour
     private IEnumerator FuseCoroutine()
     {
         yield return new WaitForSeconds(fuseTime);
+
         Explode();
     }
 
-    public void Explode()
+    private void Explode()
+    {
+        switch (bombType)
+        {
+            case BombType.SphereBomb:
+                SphereExplode();
+                break;
+            case BombType.LinesBomb:
+                LinesExplode();
+                break;
+        }
+    }
+
+    private void LinesExplode()
+    {
+        ExplodeInLine(Vector3.left);
+        ExplodeInLine(Vector3.right);
+        ExplodeInLine(Vector3.forward);
+        ExplodeInLine(Vector3.back);
+        Destroy(gameObject);
+    }
+
+    private void ExplodeInLine(Vector3 direction)
+    {
+        var hits = Physics.RaycastAll(transform.position, direction, radius)
+            .OrderBy(hit => hit.distance).ToArray();
+
+        foreach (var hit in hits)
+        {
+            var HitGameObject = hit.collider.gameObject;
+            if (HitGameObject.TryGetComponent<WallBehaviour>(out var wallBehaviour))
+            {
+                wallBehaviour.HitByExplosion();
+                return;
+            }
+        }
+    }
+
+    private void SphereExplode()
     {
         Collider[] hits = Physics.OverlapSphere(transform.position, radius);
 
@@ -30,4 +78,10 @@ public class BombExplode : MonoBehaviour
 
         Destroy(gameObject);
     }
+}
+
+public enum BombType
+{
+    SphereBomb,
+    LinesBomb
 }
