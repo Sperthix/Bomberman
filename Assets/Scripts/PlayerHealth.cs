d78331eeddbd987696ee9194f95a9b6aed489859
Filip Agh <filip11agh@gmail.com>
2025-12-27 01:03:51 +0100

    dead code

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,48 +1,22 @@
-using System;
 using Unity.Netcode;
-using UnityEngine;
 
 public class PlayerHealth : NetworkBehaviour
 {
     private static int maxHealth = 3;
-    public int MaxHealth => maxHealth;
     public NetworkVariable<int> currentHealth = new NetworkVariable<int>(3);
 
-    
-    public event Action<int, int> OnHealthChanged;
-
-    private void Start()
-    {
-        if (IsOwner)
-        {
-            currentHealth.OnValueChanged += (oldValue, newValue) =>
-            NotifyHealthChanged();
-        }
-    }
 
     public void TakeDamage(int damage)
     {
         currentHealth.Value -= damage;
-        NotifyHealthChanged();
         if (currentHealth.Value <= 0)
         {
             Die();
         }
     }
-    
-    public void Heal(int amount)
-    {
-        currentHealth.Value = Mathf.Min(maxHealth, currentHealth.Value + amount);
-        NotifyHealthChanged();
-    }
 
     private void Die()
     {
         GameManager.Instance.PlayerDied();
-    }
-
-    private void NotifyHealthChanged()
-    {
-        OnHealthChanged?.Invoke(currentHealth.Value, maxHealth);
     }
 }
